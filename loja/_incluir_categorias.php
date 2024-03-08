e682f66821a442f6b9991a6969bacba691dd61e7
Guilherme Rocha <113448929+GuiRL-dev@users.noreply.github.com>
2024-03-07 21:58:07 -0300

    Arquivos do projeto de PHP

---
+++ loja/_incluir_categorias.php
@@ -0,0 +1,21 @@
+<?php
+include 'conexao.php';
+$desc_categoria = $_POST['desc_categoria'];
+$sql = "INSERT INTO categorias (desc_categoria) VALUES ('$desc_categoria')";
+$inserir = mysqli_query($conexao, $sql);
+?>
+<head>
+  <style>
+    div{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+</head>
+<div>
+  <h4>Categoria Adicionada com Sucesso</h4>
+</div>
+<div>
+<a href="lista_categorias.php">Voltar</a>
+</div>
