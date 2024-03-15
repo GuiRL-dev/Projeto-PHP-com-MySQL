d7743fe972b4b1d429f1515e0bc18277aa55e0c2
Guilherme Rocha <113448929+GuiRL-dev@users.noreply.github.com>
2024-03-14 22:06:42 -0300

    Add files via upload

---
+++ loja/_incluir_produtos.php
@@ -0,0 +1,26 @@
+<?php
+include("conexao.php");
+$desc_produto = $_POST['desc_produto'];
+$id_categoria = $_POST['id_categoria'];
+$id_fornecedor = $_POST['id_fornecedor'];
+$preco = $_POST['preco'];
+$imagem = $_POST['imagem'];
+
+$sql="INSERT INTO produtos (desc_produto, id_categoria, id_fornecedor, preco, imagem) VALUES ('$desc_produto','$id_categoria','$id_fornecedor','$preco','$imagem')";
+$inserir = mysqli_query($conexao, $sql);
+?>
+<head>
+  <style>
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+</head>
+<div>
+  <h4>Produto Adicionado com Sucesso</h4>
+</div>
+<div>
+  <a href="lista_produtos.php">Voltar</a>
+</div>
--- loja/incluir_produtos.php
+++ loja/incluir_produtos.php
@@ -1,38 +1,66 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<html>
+<!-- incluir_produtos.php -->
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Incluir Produtos</title>
+   <title>Inclusão de Produtoss</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-  <?php
-  include 'menu.php';
-  ?>
-  <h4>Inclusão de Produtos</h4>
-  <form action="_incluir_usuarios.php" method="post">
-    <label>Nome do Produto</label>
-    <input type="text" name="desc_produto" required>
-    <label>Categoria</label>
-    <select name="id_categoria" class="form-control">
-      <option value="1">Camisetas</option>
-      <option value="2">Calças</option>
-      <option value="3">Moletons</option>
-      <option value="4">Conjuntos</option>
-      <option value="5">Toucas</option>
-      <option value="6">Adesivos</option>
-    </select>
-    <label>Senha</label>
-    <input type="text" name="password" required>
-    <button type="password" id="botao">Cadastrar</button>
-  </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+   <?php
+   include 'conexao.php';
+   include 'menu.php';
+   ?>
+   <div class="container">
+      <h4>Inclusão de Produtos</h4>
+      <!-- tag form = formulário do html, action vai enviar os dados para outra página  -->
+      <form action="_incluir_produtos.php" method="post">
+         <label>Descrição</label>
+         <input type="text" name="desc_produto" class="form-control" required>
+         <label>Categoria</label>
+         <select name="id_categoria" class="form-control">
+            <?php
+            $sql = "select * from categorias order by desc_categoria";
+            $busca_categoria = mysqli_query($conexao, $sql);
+            while ($array = mysqli_fetch_array($busca_categoria)) {
+               $id_categoria = $array['id_categoria'];
+               $desc_categoria = $array['desc_categoria'];
+               ?>
+               <option value="<?php echo ("$id_categoria") ?>">
+                  <?php echo ("$desc_categoria") ?>
+               </option>
+               <?php
+            }
+            ?>
+         </select>
+         <label>Fornecedor</label>
+         <select name="id_fornecedor" class="form-control">
+            <?php
+            $sql = "select * from fornecedores order by nome_fornecedor";
+            $busca_fornecedor = mysqli_query($conexao, $sql);
+            while ($array = mysqli_fetch_array($busca_fornecedor)) {
+               $id_fornecedor = $array['id_fornecedor'];
+               $nome_fornecedor = $array['nome_fornecedor'];
+               ?>
+               <option value="<?php echo ("$id_fornecedor") ?>">
+                  <?php echo ("$nome_fornecedor") ?>
+               </option>
+               <?php
+            }
+            ?>
+         </select>
+         <label>Preço</label>
+         <input type="text" name="preco" class="form-control">
+         <label>Imagem</label>
+         <input type="file" name="imagem" class="form-control btn btn primary" required>
+         <!-- input é entrada de dados. required, torna obrigatória a digitação -->
+         <button type="submit" id="botao" class="btn btn-primary">Cadastrar</button>
+      </form>
+   </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+      </script>
 </body>
 
 </html>
