132c626c137c865895b2a242e65a28e690a658d9
Guilherme Rocha <113448929+GuiRL-dev@users.noreply.github.com>
2024-03-22 12:22:01 -0300

    Delete loja directory

--- loja/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername="localhost";
-$database="loja";
-$username="root";
-$password="";
-
-$conexao=mysqli_connect($servername, $username, $password, $database);
-if(mysqli_connect_errno()){echo "". mysqli_connect_error();}
